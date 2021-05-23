25f2bbdbd6f8364bff056d0176d8cf0b6c58ef7f
sebas rivera <bostjanrivera@gmail.com>
2021-05-23 17:42:02 -0500

    home changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,10 @@ const Home = () => {
   const view = `
     <div class="main-home">
       <section class="home-text">
-        <h1>Hi, I'm Sebastian Rivera, I'm a Frontend developer and student of Platzi Master cohort 7</h1>
+        <h1>Hi, I'm Sebastian Rivera <br><span>Frontend Developer</span> <br><span>Platzi Master C7 Student</span></h1>
       </section>
       <figure class="home-img">
-        <img src="https://i.ibb.co/n6GvqBZ/foto-yo.jpg" alt="foto perfil Sebastian Rivera">
+        <img src="https://i.ibb.co/SrpVG7Y/foto-yo.jpg" alt="foto perfil Sebastian Rivera">
       </figure>
     </div>
   `;
